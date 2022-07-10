455ee675c5dd51578348a8f6643d4992227d808b
Mihir Chauhan <39073758+mihir-chauhan@users.noreply.github.com>
2022-07-10 15:24:32 -0700

    Work In progress (Bug with uploading oom)

--- unityInstanceHandler.js
+++ unityInstanceHandler.js
@@ -1,51 +1,76 @@
 UnityInstance = null;
-
-function writeMotorPowers() { }
-
-function fileToByteArray(file) {
-    return new Promise((resolve, reject) => {
-        try {
-            let reader = new FileReader();
-            let fileByteArray = [];
-            reader.readAsArrayBuffer(file);
-            reader.onloadend = (evt) => {
-                if (evt.target.readyState == FileReader.DONE) {
-                    let arrayBuffer = evt.target.result,
-                        array = new Uint8Array(arrayBuffer);
-                    for (byte of array) {
-                        fileByteArray.push(byte);
-                    }
-                }
-                resolve(fileByteArray);
-            }
+var alreadySetPlayMode = false;
+function check() {
+    if (UnityInstance != null) {
+        var playMode = localStorage.getItem('playMode');
+        if (playMode == "Autonomous" && !alreadySetPlayMode) {
+            UnityInstance.SendMessage("Main Menu", "changeSinglePlayer");
+            alreadySetPlayMode = true;
+        } else if (playMode == "TeleOp" && !alreadySetPlayMode) {
+            // alert("VRS Multiplayer is optimized with fullscreen mode. Please click on the blue button below the game window.");
+            alreadySetPlayMode = true;
         }
-        catch (e) {
-            reject(e);
+        if (playMode == "Autonomous") {
+            setTimeout(writeMotorPowers, 1);
         }
-    })
+    } else {
+        setTimeout(check, 500);
+    }
 }
 
-async function getByteArray() {
-    var myLog = new File([], "/custom_robots/shooterbot/unitycomp_0.stl");
-    //Wait for the file to be converted to a byteArray
-    let byteArray = await fileToByteArray(myLog);
+check();
 
-    //Do something with the byteArray
-    console.log("btyeArrya: " + byteArray);
-}
-getByteArray();
+function writeMotorPowers() {
+    if (localStorage.getItem('startMatch') == 'true') {
+        UnityInstance.SendMessage("FieldManager", "buttonStartGame");
+        localStorage.setItem('startMatch', false);
+    } else if (localStorage.getItem('stopMatch') == 'true') {
+        UnityInstance.SendMessage("FieldManager", "buttonStopGame");
+        localStorage.setItem('stopMatch', false);
+    } else if (localStorage.getItem('resetField') == 'true') {
+        UnityInstance.SendMessage("FieldManager", "resetField");
+        localStorage.setItem('resetField', false);
+    }
+
+    var motors = JSON.parse(localStorage.getItem('motorPowers'));
+    var encoderResets = JSON.parse(localStorage.getItem("motorResetEncoders"));
+    var servos = JSON.parse(localStorage.getItem('servoPositions'));
+    for (var i = 0; i < motors.length; i++)
+        if (!motors[i])
+            motors[i] = 0;
+    for (var i = 0; i < servos.length; i++)
+        if (!servos[i])
+            servos[i] = 0;
+
+    localStorage.setItem('motorResetEncoders', "[false, false, false, false, false, false, false, false]");
 
 
-function sendFilesToUnity() {
-    var myLog = new File("/custom_robots/shooterbot/unitycomp_0.stl");
-    UnityInstance.SendMessage("RobotManager", "recieveRobotFile", ["unitycomp_0.stl", ""]);
-    UnityInstance.SendMessage("RobotManager", "recieveRobotFile", ["unitycomp_1.stl", ""]);
-    UnityInstance.SendMessage("RobotManager", "recieveRobotFile", ["unitycomp_6.stl", ""]);
-    UnityInstance.SendMessage("RobotManager", "recieveRobotFile", ["unitycomp_7.stl", ""]);
-    UnityInstance.SendMessage("RobotManager", "recieveRobotFile", ["wheel_2.stl", ""]);
-    UnityInstance.SendMessage("RobotManager", "recieveRobotFile", ["wheel_3.stl", ""]);
-    UnityInstance.SendMessage("RobotManager", "recieveRobotFile", ["wheel_4.stl", ""]);
-    UnityInstance.SendMessage("RobotManager", "recieveRobotFile", ["wheel_5.stl", ""]);
-    UnityInstance.SendMessage("RobotManager", "recieveRobotFile", ["robotFile.urdf", ""]);
+    //Old Code (Lean off of using this)
+    // for (var i = 0; i < encoderResets.length; i++)
+    //     if (encoderResets[i] == true)
+    //         UnityInstance.SendMessage("PhotonNetworkPlayer(Clone)", "resetEncoders");
+    UnityInstance.SendMessage("RobotManager", "setFrontLeftVel", motors[0]);
+    UnityInstance.SendMessage("RobotManager", "setFrontRightVel", motors[1]);
+    UnityInstance.SendMessage("RobotManager", "setBackLeftVel", motors[2]);
+    UnityInstance.SendMessage("RobotManager", "setBackRightVel", motors[3]);
+    // UnityInstance.SendMessage("PhotonNetworkPlayer(Clone)", "setMotor5", motors[4]);
+    // UnityInstance.SendMessage("PhotonNetworkPlayer(Clone)", "setMotor6", motors[5]);
+    // UnityInstance.SendMessage("PhotonNetworkPlayer(Clone)", "setMotor7", motors[6]);
+    // UnityInstance.SendMessage("PhotonNetworkPlayer(Clone)", "setMotor8", motors[7]);
+    //Old Code (Lean off of using this)
 
+
+    var command = new Object();
+    command.motors = motors;
+    command.encoderResets = encoderResets;
+    command.servos = servos;
+    //To add more use: obj.<name> = array
+
+    //WIP - Unity will need to respond to this one command and set values accordingly
+    // UnityInstance.SendMessage("PhotonNetworkPlayer(Clone)", "receiveInfo", JSON.stringify(command));
+    //Sends the info: '{"motors":[0,0,0,0,0,0,0,0],"encoderResets":[false,false,false,false,false,false,false,false],"servos":[0,0,0]}'
+
+    //Implement Servos once Unity is ready
+
+    check();
 }
